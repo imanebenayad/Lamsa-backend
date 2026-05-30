bc80f799b9233577aecf2770b40d7bbba1418a27
iman <imanebenayad00@gmail.com>
2026-05-30 23:27:21 +0100

    first commit - backend setup

--- Lamsa-backend/src/main/java/com/Lamsa/controller/UserController.java
+++ Lamsa-backend/src/main/java/com/Lamsa/controller/UserController.java
@@ -1,4 +1,31 @@
 package com.Lamsa.controller;
 
+import com.Lamsa.entity.User;
+import com.Lamsa.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // CREATE USER
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    // GET ALL USERS
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
